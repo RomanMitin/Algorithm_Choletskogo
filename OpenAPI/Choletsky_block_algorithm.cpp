dba9c2cdf603359ef2842bac664bc980a3c336fe
RomanMitin <ramonmitin290502@yandex.ru>
2021-12-16 21:52:12 +0300

    Add_mkl_mult_and_report

--- OpenAPI/Choletsky_block_algorithm.cpp
+++ OpenAPI/Choletsky_block_algorithm.cpp
@@ -1,132 +1,7 @@
 #include<cmath>
 #include<algorithm>
+#include<mkl.h>
 #include"Choletsky_block_algorithm.h"
-
-Matrix Cholesky_decomposition_block(const Matrix &mat) noexcept
-{
-
-	int64_t block_size = 196;
-
-	Matrix result(mat);
-
-	int64_t shift;
-	for (shift = 0; shift < int64_t(result.sizer() - block_size); shift += block_size)
-	{
-		// Обычный алгоритм Холетского для блока L11
-		result[shift][shift] = sqrt(result[shift][shift]);
-
-		//#pragma omp parallel for
-		for (int64_t i = 1; i < block_size; i++)
-		{
-			result[i + shift][shift] = result[i + shift][shift] / result[shift][shift];
-		}
-
-		for (int64_t i = 1; i < block_size; i++)
-		{
-			type sum1 = 0;
-			//#pragma omp SIMD reduction(+: sum1)
-			for (int64_t j = 0; j < i; j++)
-			{
-				sum1 += result[i+ shift][j + shift] * result[i + shift][j + shift];
-			}
-
-			result[i + shift][i + shift] = sqrt(result[i + shift][i + shift] - sum1);
-
-			//#pragma omp parallel for
-			for (int64_t j = i + 1; j < block_size; j++)
-			{
-				type sum = 0;
-				for (int64_t p = 0; p < i; p++)
-				{
-					sum += result[i + shift][p + shift] * result[j + shift][p + shift];
-				}
-				result[j + shift][i + shift] = (result[j + shift][i + shift] - sum) / result[i + shift][i + shift];
-			}
-		}
-
-	
-		// Решение нижнетреугольной системы для нахождения блока L21
-		for (int64_t i = 0; i < block_size; i++)
-		{
-			#pragma omp parallel for
-			for (int64_t j = 0; j < result.sizer() - block_size - shift; j++)
-			{
-				type tmp = result[j + block_size + shift][i + shift];
-				result[j + block_size + shift][i + shift] = 0;
-				for (size_t k = 0; k < i; k++)
-				{
-					result[j + block_size + shift][i + shift] += \
-						result[i + shift][k + shift] * result[j + block_size + shift][k + shift];
-				}
-
-				result[j + block_size + shift][i + shift] =\
-					tmp - result[j + block_size + shift][i + shift];
-
-				result[j + block_size + shift][i + shift] /= result[i + shift][i + shift];
-			}
-		}
-
-
-	
-		// Нахождение редуцированной матрицы A22 с помощью вычитания из исходной матрицы A22 блока L21 "В квадрате"
-		#pragma omp parallel for
-		for (int64_t i = 0; i < result.sizer() - block_size - shift; i++)
-		{
-			for (int64_t j = 0; j < result.sizec() - block_size - shift; j++)
-			{
-				type elem = 0;
-				for (int64_t k = 0; k < block_size; k++)
-				{
-					elem += result[i + block_size + shift][k + shift] * result[j + block_size + shift][k + shift];
-				}
-				result[i + block_size + shift][j + block_size + shift] -= elem;
-			}
-		} 
-	}
-
-	block_size = result.sizer() - shift;
-
-	result[shift][shift] = sqrt(result[shift][shift]);
-
-	//Обычный алгоритм Холетского для последнего блока 
-	for (int64_t i = 1; i < block_size; i++)
-	{
-		result[i + shift][shift] = result[i + shift][shift] / result[shift][shift];
-	}
-
-	for (int64_t i = 1; i < block_size; i++)
-	{
-		type sum1 = 0;
-		
-		for (int64_t j = 0; j < i; j++)
-		{
-			sum1 += result[i + shift][j + shift] * result[i + shift][j + shift];
-		}
-
-		result[i + shift][i + shift] = sqrt(result[i + shift][i + shift] - sum1);
-
-		//#pragma omp parallel for
-		for (int64_t j = i + 1; j < block_size; j++)
-		{
-			type sum = 0;
-			for (int64_t p = 0; p < i; p++)
-			{
-				sum += result[i + shift][p + shift] * result[j + shift][p + shift];
-			}
-			result[j + shift][i + shift] = (result[j + shift][i + shift] - sum) / result[i + shift][i + shift];
-		}
-	}
-	
-	#pragma omp parallel for
-	for (int i = 0; i < result.sizec() - 1; i++)
-	{
-		for (int j = i + 1; j < result.sizer(); j++)
-		{
-			result[i][j] = 0;
-		}
-	}
-	return result;
-}
 
 Matrix Cholesky_decomposition_block_with_matrixblock_mult(const Matrix& mat, int64_t block_size, int block_sz_n, int block_sz_m) noexcept
 {
@@ -189,11 +64,13 @@ Matrix Cholesky_decomposition_block_with_matrixblock_mult(const Matrix& mat, int
 			}
 		}
 
-
-		// Нахождение редуцированной матрицы A22 с помощью вычитания из исходной матрицы A22 блока L21 "В квадрате"
 		int n1 = result.sizec() - block_size - shift;
 		int m1 = block_size;
 		int m2 = n1;
+		// Нахождение редуцированной матрицы A22 с помощью вычитания из исходной матрицы A22 блока L21 "В квадрате"
+		cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n1, m2, m1, 1.0, result[block_size + shift] + shift, result.sizec(), \
+			result[block_size + shift] + shift, result.sizec(), -1.0, result[block_size + shift] + shift + block_size, result.sizec());
+		/*
 		#pragma omp parallel for
 		for (int ib = 0; ib < n1; ib += block_sz_n)
 			for (int kb = 0; kb < m1; kb += block_sz_m)
@@ -208,7 +85,7 @@ Matrix Cholesky_decomposition_block_with_matrixblock_mult(const Matrix& mat, int
 								sum2 += result[i + block_size + shift][k + shift] * result[j + block_size + shift][k + shift];
 							}
 							result[i + block_size + shift][j + block_size + shift] -= sum2;
-						}
+						}*/
 	}
 
 	block_size = result.sizer() - shift;
